c1352ed8d1733d40aadcf7091e74e1f7672053da
Rae Lovejoy <git@r43.tech>
2026-08-11 23:30:08 -0700

    Assemble Help Desk visitor-services script

---
+++ js/helpdesk.js
@@ -0,0 +1,7 @@
+---
+layout: null
+---
+{% include current/helpdesk-js-00.html %}
+{% include current/helpdesk-js-01.html %}
+{% include current/helpdesk-js-02.html %}
+{% include current/helpdesk-js-03.html %}
